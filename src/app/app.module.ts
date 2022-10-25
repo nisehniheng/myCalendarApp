b4be347882b59a88ecdd1498137a03453c3475e1
nisehniheng <s64309010029@ptnicec.ac.th>
2022-10-25 18:48:42 +0700

    ส่งงานครั้งที่1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Calendar } from '@ionic-native/calendar';
+ 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddEventPage } from '../pages/add-event/add-event';
+import { EditEventPage } from '../pages/edit-event/edit-event';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddEventPage,
+    EditEventPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +25,15 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddEventPage,
+    EditEventPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Calendar
   ]
 })
 export class AppModule {}
